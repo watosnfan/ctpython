86c4e61b245df2a9c8cc73aded1d1de23cf81e32
watosnfan <30586391+watosnfan@users.noreply.github.com>
2021-03-14 16:25:28 +0800

    Add files via upload

---
+++ Ex01.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 14 09:42:45 2021
+
+@author: user
+"""
+
+for i in range(10): #[0,1,2,3,4,5,6,7,8,9]
+    print(i)
+    print('平方：',i*i)
+    
+print('程式執行完畢')    
+
+
+for i in range(10): #[0,1,2,3,4,5,6,7,8,9]
+    print("GoGo")
+    print("Ya")
+    
+print('程式執行完畢')  
---
+++ Ex09.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 14 13:52:15 2021
+
+@author: user
+"""
+import random
+
+r = random.random()
+i = random.randint(1,50)
+
+print(r)
+print(i)
+for a in range(10):
+    print(random.randint(1,10))
